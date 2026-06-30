19caaf21fdafc70be9f32146884aba708bc8c6b0
Lucas <lucas@example.com>
2026-06-30 19:58:58 -0300

    mensagem do commit

--- src/componentes/Layout.jsx
+++ src/componentes/Layout.jsx
@@ -3,7 +3,7 @@ import { Outlet, Link } from 'react-router-dom'
 export default function Layout() {
   return (
     <div className="pagina">
-      {/* RF08 (simplificado) - botão fixo no topo em todas as telas, leva pra home */}
+      
       <Link to="/" className="botao-home">
         ☰ início
       </Link>
--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -6,7 +6,6 @@ export default function Home() {
       <h1>projeto kanban</h1>
       <p>organize suas tarefas em quadros simples e visuais.</p>
 
-      {/* RF03 - botões distintos, cada um leva pra uma url diferente */}
       <div className="acoes-home">
         <Link to="/login" className="botao botao-secundario">entrar</Link>
         <Link to="/cadastro" className="botao botao-primario">criar conta</Link>
